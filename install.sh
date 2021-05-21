c7358fcd526a51f498cb6797a363a5254bd7bb29
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-21 20:53:17 +0900

    fix

--- install.sh
+++ install.sh
@@ -8,38 +8,45 @@ echo "$passwd" | sudo -S apt update
 
 # zsh
 if [ -z "$ZSH_VERSION" ]; then
-  echo "### zsh ###"
-  sudo -S apt install zsh
+  echo "### install zsh ###"
+  sudo apt install -y zsh
   chsh -s $(which zsh)
 fi
 
 # zinit
-echo "### zinit ###"
+echo "### install zinit ###"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
 source $HOME/.zshrc
 zinit self-update
 
 # asdf
-echo "### asdf ###"
+echo "### install asdf ###"
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
 
+# curl
+if !(type "curl" > /dev/null 2>&1); then
+  echo "### install curl ###"
+  sudo apt install -y curl
+fi
+
 # yarn
-echo "### yarn ###"
-sudo -S apt install --no-install-recommends yarn
+echo "### install yarn ###"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install -y --no-install-recommends yarn
 
 # unzip
 if !(type "unzip" > /dev/null 2>&1); then
-  echo "### unzip ###"
-  sudo apt install unzip
+  echo "### install unzip ###"
+  sudo apt install -y unzip
 fi
 
 # ngrok
-echo "### ngrok ###"
+echo "### install ngrok ###"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 sudo mv ngrok /usr/bin/ngrok/
-rm ngrok-stable-linux-amd64.zip
-rm -rf ngrok-stable-linux-amd64
+rm ngrok-stable-linux-amd64.zip ngrok
 
 # link
 echo "### create symbolic link ###"
@@ -54,12 +61,13 @@ zcompile $HOME/.zshrc
 source $HOME/.zshrc
 
 # asdf plugin
-echo "### asdf plugin (nodejs) ###"
+echo "### add asdf plugin (nodejs) ###"
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 
-echo "### asdf plugin (golang) ###"
+echo "### add asdf plugin (golang) ###"
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 
 echo "### finish! ###"
-exec $SHELL -l
+
+exec $(which zsh) -l
