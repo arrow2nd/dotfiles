dd621262a728b80498fb492330b4e2847e6fa772
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-21 21:02:57 +0900

    add

--- install.sh
+++ install.sh
@@ -68,6 +68,16 @@ bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-ke
 echo "### add asdf plugin (golang) ###"
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 
-echo "### finish! ###"
+# node.js
+echo "### install node.js ###"
+asdf install nodejs lts
+asdf global nodejs lts
+
+# golang
+echo "### install golang ###"
+asdf install golang latest
+asdf global golang latest
+
+echo "--- finish! ----"
 
 exec $(which zsh) -l
