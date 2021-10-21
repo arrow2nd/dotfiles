d6be3fd8a9ccf96d3c2bc6d79844343420bedf5b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-10-22 00:18:37 +0900

    add: deno

--- install-lang.sh
+++ install-lang.sh
@@ -2,24 +2,27 @@
 
 . scripts/common_func.sh
 
-# asdf plugin
-echo_title "Add asdf plugin (nodejs)"
+# nodejs
+echo_title "Add nodejs"
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-echo
 
-echo_title "Add asdf plugin (golang)"
-asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
-echo
-
-# node.js
-echo_title "Install node.js"
 asdf install nodejs lts
 asdf global nodejs lts
 echo
 
+# deno
+echo_title "Add deno"
+asdf plugin-add deno https://github.com/asdf-community/asdf-deno.git
+
+asdf install deno latest
+asdf global deno latest
+echo
+
 # golang
-echo_title "Install golang"
+echo_title "Add golang plugin"
+asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
+
 asdf install golang latest
 asdf global golang latest
 echo
