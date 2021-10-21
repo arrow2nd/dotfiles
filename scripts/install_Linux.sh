0cdd81cffbc4789cf354a1dc16ba3f7181cc45cb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-10-22 01:07:47 +0900

    add: gh for Linux

--- scripts/install_Linux.sh
+++ scripts/install_Linux.sh
@@ -45,4 +45,8 @@ rm ngrok-stable-linux-amd64.zip
 echo
 
 # gh
-install "gh"
+echo_title "Install GitHub CLI"
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh
