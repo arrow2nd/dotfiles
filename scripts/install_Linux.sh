a13f97d136f47f9772d8b153ed45f0cdec6e651c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-31 14:31:52 +0900

    feat: yay

--- scripts/install_Linux.sh
+++ scripts/install_Linux.sh
@@ -4,7 +4,20 @@
 # for Arch Linux
 #
 
+# yayをインストール
+if type "yay" > /dev/null 2>&1; then
+  echo "yay is exist!"
+else
+  echo_title "Install yay"
+  git clone https://aur.archlinux.org/yay-bin yay
+  cd yay
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay
+fi
+
 # 諸々インストール
+echo_title "Install all the usual tools"
 yay -S \
  zsh \
  unzip \
@@ -20,4 +33,5 @@ yay -S \
  ngrok
 
 # zshに切り替え
+echo_title "Switch to zsh"
 sudo chsh -s $(which zsh) $(whoami)
