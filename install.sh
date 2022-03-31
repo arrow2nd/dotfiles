5b02f1453c20f5bbab3da2c17aa9f662fc0551f7
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-31 10:48:31 +0900

    fix: zinitのURLを修正

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ done
 
 # zinit
 echo_title "Install zinit"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+sh -c "$(curl -fsSL https://git.io/zinit-install)"
 source $HOME/.zshrc
 zinit self-update
 
