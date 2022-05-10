09b8e8d7f6766e598c326c4b390e9d048efa52d1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-10 20:27:55 +0900

    feat(install): 言語のインストール処理を統合

--- install.sh
+++ install.sh
@@ -15,16 +15,30 @@ sh -c "$(curl -fsSL https://git.io/zinit-install)"
 source $HOME/.zshrc
 zinit self-update
 
-# asdf
-echo_title "Install asdf"
-git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.9.0
-
 # dein.vim
 echo_title "Install dein.vim"
 mkdir -p ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
 rm installer.sh
+
+# asdf
+echo_title "Install asdf"
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.9.0
+
+# nodejs
+echo_title "Add nodejs"
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+asdf install nodejs lts
+asdf global nodejs lts
+
+# yarn
+echo_title "Add yarn"
+npm install -g yarn
+
+# yarnでインストールしてるツール
+yarn global add joplin
 
 # zcompile
 zcompile $HOME/.zshrc
