b1d2697dfb2dabaed6d132f0e6024bfee6fe09f8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-07-10 17:59:59 +0900

    fix(kitty): 条件を以上に修正

--- kitty/.config/kitty/tab_bar.py
+++ kitty/.config/kitty/tab_bar.py
@@ -72,9 +72,9 @@ def _get_battery_status(default: Color) -> tuple[Color, str]:
 
     # 残量から表示色を決定
     color = default
-    if batt < 10:
+    if batt <= 10:
         color = danger
-    elif batt < 20:
+    elif batt <= 20:
         color = warn
 
     # 残量に合ったアイコンを選択
