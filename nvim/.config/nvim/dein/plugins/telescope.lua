1fae05eb12af3386e863bd147f5a49dab569731f
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-11 20:12:20 +0900

    feat(nvim): telescopeの設定を変更
    - 表示幅を拡大
    - `.git/` を常に除外
    - 隠しファイルを検索対象に含める

--- nvim/.config/nvim/dein/plugins/telescope.lua
+++ nvim/.config/nvim/dein/plugins/telescope.lua
@@ -2,29 +2,40 @@ local ok, telescope = pcall(require, 'telescope')
 
 if (not ok) then return end
 
-local builtin = require("telescope.builtin")
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
 
 telescope.setup{
   defaults = {
     mappings = {
-      i = {
-        ["<esc>"] = require('telescope.actions').close,
+      n = {
+        ["q"] = actions.close,
       },
     },
+    layout_config = {
+      horizontal = {
+        width = 0.9,
+      },
+    },
+    file_ignore_patterns = {
+      '^.git/',
+    }
   },
-  pickers = {},
-  extensions = {}
 }
 
 vim.keymap.set('n', '<Leader>ff',
   function()
-    builtin.find_files()
+    builtin.find_files({
+      hidden = true,
+    })
   end
 )
 
 vim.keymap.set('n', '<Leader>fg',
   function()
-    builtin.live_grep()
+    builtin.live_grep({
+      hidden = true,
+    })
   end
 )
 
