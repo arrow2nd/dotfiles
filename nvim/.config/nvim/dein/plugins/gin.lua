3b6c7bdae374a620486e216a9421d73a3e1fd8dc
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-13 12:57:35 +0900

    fix(nvim/gin): 衝突する設定を削除

--- nvim/.config/nvim/dein/plugins/gin.lua
+++ nvim/.config/nvim/dein/plugins/gin.lua
@@ -1,13 +1,9 @@
 local helper = require('helper')
 
-vim.api.nvim_set_var('gin_status_default_args', { '++opener=split' })
-vim.api.nvim_set_var('gin_diff_default_args', { '++opener=vsplit' })
-vim.api.nvim_set_var('gin_patch_default_args', { '++opener=vsplit' })
-
-helper.nmap('<Leader>gs', '<CMD>GinStatus<CR>')
+helper.nmap('<Leader>gs', '<CMD>GinStatus ++opener=split<CR>')
 helper.nmap('<Leader>gl', '<CMD>GinBuffer ++opener=split log<CR>')
-helper.nmap('<Leader>gd', '<CMD>GinDiff<CR>')
+helper.nmap('<Leader>gd', '<CMD>GinDiff ++opener=vsplit<CR>')
 
 helper.nmap('<Leader>gc', '<CMD>Gin commit -v<CR>')
-helper.nmap('<Leader>gp', '<CMD>GinPatch<CR>')
+helper.nmap('<Leader>gp', '<CMD>GinPatch ++opener=vsplit<CR>')
 
