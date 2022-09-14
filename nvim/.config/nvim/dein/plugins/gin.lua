e2f1b3cd32dd885b16db0147c80c39eb4b8ea983
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-14 13:46:53 +0900

    feat(nvim): 不要な設定ファイルを削除

--- nvim/.config/nvim/dein/plugins/gin.lua
+++
@@ -1,9 +0,0 @@
-local helper = require('helper')
-
-helper.nmap('<Leader>gs', '<CMD>GinStatus ++opener=split<CR>')
-helper.nmap('<Leader>gl', '<CMD>GinBuffer ++opener=split log<CR>')
-helper.nmap('<Leader>gd', '<CMD>GinDiff ++opener=vsplit<CR>')
-
-helper.nmap('<Leader>gc', '<CMD>Gin commit -v<CR>')
-helper.nmap('<Leader>gp', '<CMD>GinPatch ++opener=vsplit<CR>')
-
