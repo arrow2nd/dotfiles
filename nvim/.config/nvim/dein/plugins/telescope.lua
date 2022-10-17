e273417fd6f0f1dc889b368ecac4aecf34f2bc9d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-10-17 11:34:52 +0900

    feat(nvim/telescope): live_grep の検索に隠しファイルを含める

--- nvim/.config/nvim/dein/plugins/telescope.lua
+++ nvim/.config/nvim/dein/plugins/telescope.lua
@@ -24,6 +24,16 @@ telescope.setup {
     file_ignore_patterns = {
       '^.git/',
     },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+    },
   },
   pickers = {
     find_files = {
