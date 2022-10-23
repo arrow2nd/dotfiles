36e56132f1c696f8bc55bf06c61a432ea0c0da62
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-10-23 14:29:21 +0900

    refactor(nvim): ifの括弧を削除

--- nvim/.config/nvim/dein/plugins/lsp.lua
+++ nvim/.config/nvim/dein/plugins/lsp.lua
@@ -1,20 +1,20 @@
 local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
-if (not lspconfig_ok) then return end
+if not lspconfig_ok then return end
 
 local mason_ok, mason = pcall(require, 'mason')
-if (not mason_ok) then return end
+if not mason_ok then return end
 
 local mason_lspconfig_ok, mason_lspconfig = pcall(require, 'mason-lspconfig')
-if (not mason_lspconfig_ok) then return end
+if not mason_lspconfig_ok then return end
 
 local cmp_ok, cmp = pcall(require, 'cmp')
-if (not cmp_ok) then return end
+if not cmp_ok then return end
 
 local cmp_nvim_lsp_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
-if (not cmp_nvim_lsp_ok) then return end
+if not cmp_nvim_lsp_ok then return end
 
 local null_ls_ok, null_ls = pcall(require, 'null-ls')
-if (not null_ls_ok) then return end
+if not null_ls_ok then return end
 
 local h = require('helper')
 
