850f42da567f1988d2ede3f5cf0c66a52f008e95
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-10-28 09:45:38 +0900

    feat(nvim/nvim-treesitter): 100KB以上のファイルをハイライトしない
    ref: https://github.com/nvim-treesitter/nvim-treesitter

--- nvim/.config/nvim/dein/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/dein/plugins/nvim-treesitter.lua
@@ -7,7 +7,13 @@ treesitter.setup {
   },
   highlight = {
     enable = true,
-    disable = {},
+    disable = function(_, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local _ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if _ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
   },
   indent = {
     enable = true
