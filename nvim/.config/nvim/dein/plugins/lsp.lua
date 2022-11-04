b01e9cf4b29a53c154c90db4ac64eed0222c0271
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-11-04 23:17:35 +0900

    feat(nvim/lsp): フォーマット時のタイムアウト間隔を緩和

--- nvim/.config/nvim/dein/plugins/lsp.lua
+++ nvim/.config/nvim/dein/plugins/lsp.lua
@@ -49,7 +49,7 @@ local common_on_attach = function(client, bufnr)
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd('BufWritePre', {
       callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr })
+        vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 5000 })
       end,
       group = augroup,
       buffer = bufnr,
