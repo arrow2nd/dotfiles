72c4158ff98d5870517948b07c5e866d998ece7d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-12-20 22:26:44 +0900

    feat(zsh): キーバインドを削除

--- nvim/.config/nvim/dein/plugins/telescope.lua
+++ nvim/.config/nvim/dein/plugins/telescope.lua
@@ -5,8 +5,6 @@ telescope.setup {
   defaults = {
     mappings = {
       i = {
-        ['J'] = 'move_selection_next',
-        ['K'] = 'move_selection_previous',
         ['<C-n>'] = 'cycle_history_next',
         ['<C-p>'] = 'cycle_history_prev',
         ['<ESC><ESC>'] = 'close',
