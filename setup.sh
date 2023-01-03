2a84296928e750da11cb468bf3fe88b222c5be2d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-03 21:49:49 +0900

    feat: #6 (#7)

--- setup.sh
+++ setup.sh
@@ -2,30 +2,90 @@
 
 set -eu 
 
-echo "=== clone repo ==="
-cd ~
-git clone https://github.com/arrow2nd/dotfiles.git
-cd dotfiles
+DOT_DIR="$HOME/dotfiles"
 
-[[ -f scripts/install_`uname`.sh ]] && . ./scripts/install_`uname`.sh
+MENU="dotfiles (for macOS / ArchLinux)
 
-echo "=== create symbolic links ==="
-stow -v --no-folding joplin vim nvim zsh vsvim sheldon nekome wezterm
+SETUP MENU:
+  [a] Execute all
+  [c] Clone dotfiles
+  [i] Install tools
+  [l] Link dotfiles
+  [s] Setup Git & GitHub CLI
+"
 
-echo "=== install dein ==="
-mkdir -p ~/.cache/dein
-curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh > installer.sh
-sh ./installer.sh ~/.cache/dein
-rm installer.sh
+clone_repo() {
+  echo "[ Clone dotfiles repo ]"
 
-read -p "=== setup Git? (Y/n) ===" yn
-case "$yn" in [nN]*) ;; *) . ./scripts/setup_git.sh ;; esac
+  if [ -d "$DOT_DIR" ]; then
+    echo "$DOT_DIR already exists."
+  else
+    git clone https://github.com/arrow2nd/dotfiles.git "$DOT_DIR"
+  fi
 
-read -p "=== setup GitHub CLI? (Y/n) ===" yn
-case "$yn" in [nN]*) ;; *) . ./scripts/setup_gh.sh ;; esac
+  echo ""
+}
 
-source $HOME/.zsh/.zshrc
-zcompile $HOME/.zsh/.zshrc
+install_tools() {
+  echo "[ Install tools ]"
 
-echo "=== finish! ==="
-exec $(which zsh) -l
+  local path="${DOT_DIR}/scripts/install_${uname}.sh"
+  if [[ -f "$path" ]]; then
+    sh -c "$path"
+  else
+    echo "$path does not exist."
+  fi
+
+  echo ""
+}
+
+link_dotfiles() {
+  echo "[ Link dotfiles ]"
+
+  if [ -d "$DOT_DIR" ]; then
+    for f in $(find . -type f -path '*/.*' -not -path '*.git*' -not -path '*.DS_Store' -not -path '*.luarc.json' | cut -c 3-); do
+      mkdir -p "$HOME/$(dirname "$f")"
+      if [ -L "$HOME/$f" ]; then
+        ln -sfv "$DOT_DIR/$f" "$HOME/$f"
+      else
+        ln -siv "$DOT_DIR/$f" "$HOME/$f"
+      fi
+    done
+  else
+    echo "$DOT_DIR does not exists."
+  fi
+
+  echo ""
+}
+
+setup_git() {
+  sh -c "${DOT_DIR}/scripts/setup_git.sh"
+  sh -c "${DOT_DIR}/scripts/setup_gh.sh"
+}
+
+if ! type -p git >/dev/null; then
+  echo "error: git not found on the system" >&2
+  exit 1
+fi
+
+echo "$MENU"
+read -r result
+echo ""
+
+if [[ "$result" == *"a"* ]] || [[ "$result" == *"c"* ]]; then 
+  clone_repo
+fi
+
+if [[ "$result" == *"a"* ]] || [[ "$result" == *"i"* ]]; then 
+  install_tools
+fi
+
+if [[ "$result" == *"a"* ]] || [[ "$result" == *"l"* ]]; then 
+  link_dotfiles
+fi
+
+if [[ "$result" == *"a"* ]] || [[ "$result" == *"s"* ]]; then 
+  setup_git
+fi
+
+echo "[ Finished! ]"
