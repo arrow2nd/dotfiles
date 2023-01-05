f163d453693679a64bc5a219cee27df117822571
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-05 12:48:32 +0900

    feat(nvim): denopsをVeryLazyで読み込むよう変更

--- .config/nvim/lua/plugins/denops.lua
+++ .config/nvim/lua/plugins/denops.lua
@@ -2,13 +2,18 @@ local fn = vim.fn
 
 return {
   {
+    'vim-denops/denops.vim',
+    event = 'VeryLazy',
+  },
+  {
     '4513ECHO/denops-gitter.vim',
     event = 'VeryLazy',
-    dependencies = { 'vim-denops/denops.vim' },
+    init = function()
+      vim.api.nvim_create_user_command('ReadingVimrc', 'new gitter://room/vim-jp/reading-vimrc', {})
+    end,
     config = function()
       local token = fn.readfile(fn.expand('~/.config/denops-gitter/.token'))
       vim.g['gitter#token'] = fn.trim(table.concat(token))
-      vim.api.nvim_create_user_command('ReadingVimrc', 'new gitter://room/vim-jp/reading-vimrc', {})
     end
   }
 }
