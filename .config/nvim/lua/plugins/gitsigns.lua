42dcdfcf979c51df1ade77d5371c2993d1c42c16
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-07 23:52:36 +0900

    refactor(nvim)

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,51 +1,53 @@
-return {
-  {
-    'lewis6991/gitsigns.nvim',
-    event = "BufReadPost",
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    init = function()
-      local h = require('util.helper')
-      h.nmap('<Leader>hb', '<CMD>Gitsigns blame_line<CR>', { desc = "Show git blame on the current line" })
-      h.nmap('<Leader>hp', '<CMD>Gitsigns preview_hunk<CR>', { desc = "Show preview the hunk" })
-      h.nmap('<Leader>hs', '<CMD>Gitsigns stage_hunk<CR>', { desc = "Stage the hunk" })
-      h.nmap('<Leader>hu', '<CMD>Gitsigns undo_stage_hunk<CR>', { desc = "Undo the last call of stage hunk" })
-      h.nmap('<Leader>hr', '<CMD>Gitsigns reset_hunk<CR>', { desc = "Reset the lines of the hunk" })
-    end,
-    config = {
-      signs = {
-        add = {
-          hl = 'GitSignsAdd',
-          text = '+',
-          numhl = 'GitSignsAddNr',
-          linehl = 'GitSignsAddLn',
-        },
-        change = {
-          hl = 'GitSignsChange',
-          text = '*',
-          numhl = 'GitSignsChangeNr',
-          linehl = 'GitSignsChangeLn',
-        },
-        delete = {
-          hl = 'GitSignsDelete',
-          text = '-',
-          numhl = 'GitSignsDeleteNr',
-          linehl = 'GitSignsDeleteLn',
-        },
-        topdelete = {
-          hl = 'GitSignsDelete',
-          text = '-',
-          numhl = 'GitSignsDeleteNr',
-          linehl = 'GitSignsDeleteLn',
-        },
-        changedelete = {
-          hl = 'GitSignsChange',
-          text = '*',
-          numhl = 'GitSignsChangeNr',
-          linehl = 'GitSignsChangeLn',
-        },
-      },
-    }
-  }
+local M = {
+  'lewis6991/gitsigns.nvim',
+  event = "BufReadPost",
+  dependencies = { 'nvim-lua/plenary.nvim' },
 }
+
+function M.init()
+  local h = require('util.helper')
+  h.nmap('<Leader>hb', '<CMD>Gitsigns blame_line<CR>', { desc = "Show git blame on the current line" })
+  h.nmap('<Leader>hp', '<CMD>Gitsigns preview_hunk<CR>', { desc = "Show preview the hunk" })
+  h.nmap('<Leader>hs', '<CMD>Gitsigns stage_hunk<CR>', { desc = "Stage the hunk" })
+  h.nmap('<Leader>hu', '<CMD>Gitsigns undo_stage_hunk<CR>', { desc = "Undo the last call of stage hunk" })
+  h.nmap('<Leader>hr', '<CMD>Gitsigns reset_hunk<CR>', { desc = "Reset the lines of the hunk" })
+end
+
+function M.config()
+  require("gitsigns").setup({
+    signs = {
+      add = {
+        hl = 'GitSignsAdd',
+        text = '+',
+        numhl = 'GitSignsAddNr',
+        linehl = 'GitSignsAddLn',
+      },
+      change = {
+        hl = 'GitSignsChange',
+        text = '*',
+        numhl = 'GitSignsChangeNr',
+        linehl = 'GitSignsChangeLn',
+      },
+      delete = {
+        hl = 'GitSignsDelete',
+        text = '-',
+        numhl = 'GitSignsDeleteNr',
+        linehl = 'GitSignsDeleteLn',
+      },
+      topdelete = {
+        hl = 'GitSignsDelete',
+        text = '-',
+        numhl = 'GitSignsDeleteNr',
+        linehl = 'GitSignsDeleteLn',
+      },
+      changedelete = {
+        hl = 'GitSignsChange',
+        text = '*',
+        numhl = 'GitSignsChangeNr',
+        linehl = 'GitSignsChangeLn',
+      },
+    },
+  })
+end
+
+return M
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,9 @@
+local M = {
+  'nvim-lualine/lualine.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'kyazdani42/nvim-web-devicons' },
+}
+
 local function get_lsp_progress()
   local prog = vim.lsp.util.get_progress_messages()[1]
   if not prog then return '' end
@@ -8,45 +14,40 @@ local function get_lsp_progress()
   return string.format('%s (%s%%%%)', title, per)
 end
 
-return {
-  {
-    'nvim-lualine/lualine.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'kyazdani42/nvim-web-devicons',
+function M.config()
+  require("lualine").setup({
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = { left = '|', right = '|' },
+      section_separators = { left = '', right = '' },
+      disabled_filetypes = {},
+      always_divide_middle = true,
+      globalstatus = false,
     },
-    config = {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '|', right = '|' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {},
-        always_divide_middle = true,
-        globalstatus = false,
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        'branch',
+        'diff',
+        { 'diagnostics', sources = { 'nvim_lsp' } },
       },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = {
-          'branch',
-          'diff',
-          { 'diagnostics', sources = { 'nvim_lsp' } },
-        },
-        lualine_c = { 'filename', get_lsp_progress },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = {},
-        lualine_y = { 'encoding', 'location' },
-        lualine_z = {}
-      },
-      tabline = {},
-      extensions = {}
-    }
-  }
-}
+      lualine_c = { 'filename', get_lsp_progress },
+      lualine_x = { 'encoding', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = {},
+      lualine_y = { 'encoding', 'location' },
+      lualine_z = {}
+    },
+    tabline = {},
+    extensions = {}
+  })
+end
+
+return M
