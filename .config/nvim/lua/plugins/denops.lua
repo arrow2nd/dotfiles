7422313b7b908a10046913829519b6ee00bd184c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-15 17:20:54 +0900

    feat(nvim): skkeletonを追加

--- .config/nvim/lua/plugins/denops.lua
+++ .config/nvim/lua/plugins/denops.lua
@@ -1,9 +1,29 @@
+local h = require("util.helper")
 local fn = vim.fn
 
 return {
   {
     "vim-denops/denops.vim",
     event = "VeryLazy",
+  },
+  {
+    "vim-skk/skkeleton",
+    event = "BufReadPre",
+    init = function()
+      h.imap("<C-j>", "<Plug>(skkeleton-enable)")
+      h.cmap("<C-j>", "<Plug>(skkeleton-enable)")
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "skkeleton-initialize-pre",
+        callback = function()
+          vim.fn["skkeleton#config"]({
+            eggLikeNewline = true,
+            skkServerPort = 1178,
+            useSkkServer = true,
+          })
+        end
+      })
+    end
   },
   {
     "4513ECHO/denops-gitter.vim",
