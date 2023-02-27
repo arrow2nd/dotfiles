6a46688605bf1fbe0709a5b49d9dfcd7dc82ecd5
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-02-27 14:05:53 +0900

    feat: i3 -> sway

---
+++ .config/sway/vime.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -u
+
+# Thanks! : https://gist.github.com/kuuote/4b2feda31d80fc8266fc562fc7f89c7f
+
+tmp=/tmp/vime
+[[ -f ${tmp} ]] && rm ${tmp}
+touch ${tmp}
+
+wezterm start --class Vime nvim -c start ${tmp} || exit 1
+head -c -1 ${tmp} | wl-copy
+
+notify-send -t 1000 Copied!
