2eb50545ca242cae6a9fb945f85707b0c7756557
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-01 01:47:35 +0900

    feat(linux): sway の起動スクリプト

--- scripts/Darwin/find.sh
+++ scripts/Darwin/find.sh
@@ -7,6 +7,7 @@ find . -type f \
        -not -path "*.git*" \
        -not -path "*.DS_Store" \
        -not -path "*.luarc.json" \
+       -not -path "*.local*" \
        -not -path "*wofi*" \
        -not -path "*sway*" \
        -not -path "*waybar*" | \
