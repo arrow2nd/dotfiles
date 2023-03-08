e569cf4a78d923ed1db549bcd2e06f284e35908e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-08 10:49:29 +0900

    feat(nvim): treesitterの無効化設定をコメントアウト

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,13 +7,13 @@ return {
         autotag = { enable = true },
         highlight = {
           enable = true,
-          disable = function(_, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local _ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if _ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
+          -- disable = function(_, buf)
+          --   local max_filesize = 100 * 1024 -- 100 KB
+          --   local _ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          --   if _ok and stats and stats.size > max_filesize then
+          --     return true
+          --   end
+          -- end,
         },
         indent = { enable = true },
         ensure_installed = {
