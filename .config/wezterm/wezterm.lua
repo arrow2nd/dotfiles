8b227614ca1659085e882c846c3ae03d394d4b4f
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-03-29 22:18:11 +0900

    feat(wezterm): タブを閉じる際のキーマップを変更

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local keybinds = {
   { key = 'v', mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
   -- タブ
   { key = 't', mods = 'ALT',        action = act.SpawnTab 'CurrentPaneDomain' },
-  { key = 'w', mods = 'ALT',        action = act.CloseCurrentTab { confirm = true } },
+  { key = 'c', mods = 'ALT',        action = act.CloseCurrentTab { confirm = true } },
   { key = '1', mods = 'ALT',        action = act.ActivateTab(0) },
   { key = '2', mods = 'ALT',        action = act.ActivateTab(1) },
   { key = '3', mods = 'ALT',        action = act.ActivateTab(2) },
