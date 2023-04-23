b06440add2876446cb9d8859f2935429f9cecd49
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-23 21:30:02 +0900

    🔧 prettierd -> prettier

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -125,7 +125,7 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.code_actions.gitsigns,
-          null_ls.builtins.formatting.prettierd,
+          null_ls.builtins.formatting.prettier,
           null_ls.builtins.diagnostics.textlint.with({
             filetypes = { 'markdown' },
             prefer_local = 'node_modules/.bin',
