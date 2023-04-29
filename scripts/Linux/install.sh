81732ac558006b663adb73525b6ca308481ad6a9
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-30 00:42:32 +0900

    🚑 (Linux) インストールスクリプトを修正

--- scripts/Linux/install.sh
+++ scripts/Linux/install.sh
@@ -3,26 +3,49 @@
 set -eu
 
 echo "[ Tools ]"
+
+# deno
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# golang
+sudo add-apt-repository ppa:longsleep/golang-backports
+
+# ngrok
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+
+# gh
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt update
+
 sudo apt install -y \
  zsh \
- deno \
  ffmpeg \
- go \
- goreleaser \
+ golang \
  xsel \
  ngrok \
  ripgrep \
- rustup \
  keychain \
  neovim \
- github-cli \
- ghq \
- sheldon \
+ gh \
  fzf \
  exa \
- fd \
+ fd-find \
  bat \
  trash-cli
+
+# sheldon
+cargo install sheldon
+
+# ghq
+go install github.com/x-motemen/ghq@latest
 
 echo "[ Switch to zsh ]"
 sudo chsh -s $(which zsh) $(whoami)
