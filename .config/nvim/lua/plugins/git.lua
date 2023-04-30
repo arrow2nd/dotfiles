4e10bd2b7697139a48f1d5712fd76c7646c114c3
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-30 14:50:06 +0900

    📦 (nvim) git-messenger.vim を追加

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -11,6 +11,40 @@ return {
     end
   },
   {
+    'lewis6991/gitsigns.nvim',
+    event = 'BufReadPost',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    init = function()
+      -- h.nmap('<C-k>', '<CMD>Gitsigns blame_line<CR>', { desc = 'Show git blame on the current line' })
+      h.nmap('<Leader>hp', '<CMD>Gitsigns preview_hunk<CR>', { desc = 'Show preview the hunk' })
+      h.nmap('<Leader>hs', '<CMD>Gitsigns stage_hunk<CR>', { desc = 'Stage the hunk' })
+      h.nmap('<Leader>hu', '<CMD>Gitsigns undo_stage_hunk<CR>', { desc = 'Undo the last call of stage hunk' })
+      h.nmap('<Leader>hr', '<CMD>Gitsigns reset_hunk<CR>', { desc = 'Reset the lines of the hunk' })
+    end,
+    config = function()
+      require('gitsigns').setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '*' },
+          delete = { text = '-' },
+          topdelete = { text = '-' },
+          changedelete = { text = '*' },
+        },
+        preview_config = {
+          border = 'none',
+          style = 'minimal',
+        },
+      })
+    end
+  },
+  {
+    'rhysd/git-messenger.vim',
+    cmd = 'GitMessenger',
+    init = function()
+      h.nmap('<C-k>', '<CMD>GitMessenger<CR>', { desc = 'Show git blame on the current line' })
+    end
+  },
+  {
     'sindrets/diffview.nvim',
     dependencies = {
       'nvim-lua/plenary.nvim',
