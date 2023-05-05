e1495e0bac708322a22b9611f915bd3d5183fa2d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-05 19:25:22 +0900

    🔥 (nvim) ターミナルを開くコマンドを削除

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -66,8 +66,5 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
   },
 })
 
--- Terminalを現在のウィンドウの下部に開く
-vim.api.nvim_create_user_command('T', 'split | wincmd j | resize 15 | terminal <args>', { nargs = '*' })
-
 -- 自動フォーマットを無視して保存
 vim.api.nvim_create_user_command('W', 'noautocmd w', {})
