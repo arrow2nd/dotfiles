adaa3063a22dd62570c3c6e4afbb0233e868f5fb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-06 21:43:15 +0900

    ✨ (nvim) Neogitを追加

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,12 +2,52 @@ local h = require('util.helper')
 
 return {
   {
-    'tpope/vim-fugitive',
-    cmd = { 'Git', 'Gvdiffsplit' },
+    'TimUntersberger/neogit',
+    cmd = { 'Neogit' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+    },
     init = function()
-      h.nmap('<Leader>gs', '<CMD>Git<CR>', { desc = 'git status' })
-      h.nmap('<Leader>gc', '<CMD>Git commit<CR>', { desc = 'git commit' })
-      h.nmap('<Leader>gb', '<CMD>Git blame<CR>', { desc = 'git blame' })
+      h.nmap('<Leader>gg', '<CMD>Neogit<CR>', { desc = 'Open Neogit' })
+      h.nmap('<Leader>gc', '<CMD>Neogit commit<CR>', { desc = 'git commit' })
+    end,
+    config = function()
+      require("neogit").setup({
+        disable_signs = false,
+        disable_hint = false,
+        disable_context_highlighting = false,
+        disable_commit_confirmation = false,
+        disable_insert_on_commit = false,
+        auto_refresh = true,
+        sort_branches = "-committerdate",
+        disable_builtin_notifications = false,
+        use_magit_keybindings = false,
+        kind = "tab",
+        console_timeout = 2000,
+        auto_show_console = true,
+        remember_settings = true,
+        use_per_project_settings = true,
+        ignored_settings = {},
+        commit_popup = { kind = "split" },
+        preview_buffer = { kind = "split" },
+        popup = { kind = "split" },
+        signs = {
+          section = { ">", "v" },
+          item = { ">", "v" },
+          hunk = { "", "" },
+        },
+        integrations = { diffview = true },
+        sections = {
+          untracked = { folded = false },
+          unstaged = { folded = false },
+          staged = { folded = false },
+          stashes = { folded = true },
+          unpulled = { folded = true },
+          unmerged = { folded = false },
+          recent = { folded = true },
+        },
+      })
     end
   },
   {
