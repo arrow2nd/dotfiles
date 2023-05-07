11a2c9cc515d3bc0559360ad325031e560c2e1f7
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-08 06:34:57 +0900

    👍 (nvim) 990 -> 9999

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -39,12 +39,12 @@ return {
       require('mini.statusline').setup({
         content = {
           active = function()
-            local mode, mode_hl    = MiniStatusline.section_mode({ trunc_width = 999 }) -- 常にShort表示
+            local mode, mode_hl    = MiniStatusline.section_mode({ trunc_width = 9999 }) -- 常にShort表示
             local git              = MiniStatusline.section_git({ trunc_width = 75 })
             local diagnostics      = MiniStatusline.section_diagnostics({ trunc_width = 75 })
             local filename         = MiniStatusline.section_filename({ trunc_width = 140 })
             local fileinfo         = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-            local location         = MiniStatusline.section_location({ trunc_width = 999 }) -- 常にShort表示
+            local location         = MiniStatusline.section_location({ trunc_width = 9999 }) -- 常にShort表示
 
             local get_lsp_progress = function()
               local prog = vim.lsp.util.get_progress_messages()[1]
