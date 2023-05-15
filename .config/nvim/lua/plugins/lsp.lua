55ecafd0f2c900f9782f5a2ebb4c8dffbf710448
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-15 23:19:59 +0900

    ✨ (nvim) 自動フォーマットをプロジェクト単位で無効化できるように

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,29 @@
 local h = require('util.helper')
 
+-- 自動フォーマットを有効にできるか
+--
+-- 環境変数 `NVIM_DISABLE_AUTOFORMATTING_PROJECTS` にプロジェクトのパスを追加することで無効にできます
+-- カンマ区切りで複数指定可能です
+local function enable_autoformat()
+  local root = vim.fn.getcwd(0)
+  local disable_projects = vim.split(os.getenv('NVIM_DISABLE_AUTOFORMATTING_PROJECTS') or '', ',')
+
+  -- 無効にするプロジェクトか
+  for _, project in ipairs(disable_projects) do
+    if root == project then
+      return false
+    end
+  end
+
+  return true
+end
+
 local common_on_attach = function(client, bufnr)
   -- Semantic token を有効にすると色がいっぱいになるので切る
   client.server_capabilities.semanticTokensProvider = nil
 
-  -- 保存時に自動でフォーマット
-  if client.supports_method('textDocument/formatting') then
+  -- 保存時に自動フォーマット
+  if client.supports_method('textDocument/formatting') and enable_autoformat() then
     local augroup = vim.api.nvim_create_augroup('LspFormatting', { clear = false })
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd('BufWritePre', {
