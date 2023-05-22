51b2cefc85031a3aa31a011fc7963e4292ff7f54
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-22 20:42:51 +0900

    🚑 (install) Brewfile -> .Brewfile

--- scripts/Darwin/install.sh
+++ scripts/Darwin/install.sh
@@ -8,5 +8,5 @@ if ! type -p brew >/dev/null; then
 fi
 
 echo "[ Tools ]"
-curl https://gist.githubusercontent.com/arrow2nd/22e12dad29f29ed308a9acf2e54cfd90/raw/eea4d3d4e1aa04e4de3f989ab12f4836747fe5ff/Brewfile > $HOME/Brewfile
+curl https://gist.githubusercontent.com/arrow2nd/22e12dad29f29ed308a9acf2e54cfd90/raw/eea4d3d4e1aa04e4de3f989ab12f4836747fe5ff/Brewfile > $HOME/.Brewfile
 brew bundle --global
