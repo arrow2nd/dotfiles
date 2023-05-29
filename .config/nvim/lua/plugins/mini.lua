5d1be1ed08bfeed9a5f8dac63607da583bea17fb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-29 21:21:27 +0900

    👍 (nvim) TODO NOTE のハイライト設定を削除

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -37,8 +37,6 @@ return {
         highlighters = {
           fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
           hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
-          todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
-          note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
           hex_color = hipatterns.gen_highlighter.hex_color(),
         },
