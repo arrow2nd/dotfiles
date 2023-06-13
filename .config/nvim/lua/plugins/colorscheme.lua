b0878c6af5ca7c40329ee028d30726abd310245e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-06-14 00:17:19 +0900

    👍 (nvim) コメントを斜体に

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
     config = function()
       vim.cmd([[colorscheme iceberg]])
       vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
+      vim.cmd([[hi Comment gui=italic]])
     end,
   },
 }
