3805439100ed2ff9a9b10d3c9df8f6d2985aa6c2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-07-20 00:55:23 +0900

    👍 (nvim) カラースキームを自作してみてる

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,23 @@
 return {
   {
     "cocopon/iceberg.vim",
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme iceberg]])
       vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
       vim.cmd([[hi Comment gui=italic]])
+    end,
+  },
+  {
+    "arrow2nd/aqua.vim",
+    -- dir = "~/workspace/github.com/arrow2nd/aqua.vim",
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme aqua]])
     end,
   },
 }
