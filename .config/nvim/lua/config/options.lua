746dbcb7bd2a6020ecbbeb3d3d2c9d9b02a29136
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-08-16 12:51:00 +0900

    👍 (nvim) 警告のソースが無い場合、本文のみを表示するように

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -62,6 +62,10 @@ opt.pumheight = 24
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = {
     format = function(diagnostic)
+      if not diagnostic.source then
+        return diagnostic.message
+      end
+
       return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
     end,
   },
