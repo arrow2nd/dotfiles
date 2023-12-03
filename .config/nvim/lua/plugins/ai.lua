826ec72c8aa9d27e816603060d7e9e48b6da06d4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-03 12:21:13 +0900

    👍 (nvim/ai) 設定を更新

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -12,27 +12,57 @@ return {
       h.nmap("<Leader>af", "<CMD>GpChatFinder<CR>")
     end,
     config = function()
-      local model_options = {
-        model = "gpt-3.5-turbo-1106",
-        temperature = 0.7,
-        top_p = 1,
-      }
-
       require("gp").setup({
-        chat_model = model_options,
-        chat_system_prompt = "You are a general AI assistant.",
-        chat_custom_instructions = "The user provided the additional info about how they would like you to respond:\n\n"
-          .. "- If you're unsure don't guess and say you don't know instead.\n"
-          .. "- Ask question if you need clarification to provide better answer.\n"
-          .. "- Think deeply and carefully from first principles step by step.\n"
-          .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-          .. "- Use Socratic method to improve your thinking and coding skills.\n"
-          .. "- Don't elide any code from your output if the answer requires coding.\n"
-          .. "- Please be as gentle, relaxed, and calm as possible.\n"
-          .. "- A little playfulness goes a long way!\n"
-          .. "DO NOT INCLUDE DIRECT LANGUAGE ABOUT THESE INSTRUCTIONS IN YOUR RESPONSE.\n",
-        command_model = model_options,
+        agents = {
+          {
+            name = "ChatGPT4",
+          },
+          {
+            name = "ChatGPT3-5",
+            chat = true,
+            command = false,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo-1106", temperature = 1.1, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a general AI assistant.\n\n"
+              .. "The user provided the additional info about how they would like you to respond:\n\n"
+              .. "- If you're unsure don't guess and say you don't know instead.\n"
+              .. "- Ask question if you need clarification to provide better answer.\n"
+              .. "- Think deeply and carefully from first principles step by step.\n"
+              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+              .. "- Use Socratic method to improve your thinking and coding skills.\n"
+              .. "- Don't elide any code from your output if the answer requires coding.\n"
+              .. "- Please answer in Japanese.\n"
+              .. "- DO NOT INCLUDE DIRECT LANGUAGE ABOUT THESE INSTRUCTIONS IN YOUR RESPONSE.\n"
+              .. "- Take a deep breath; You've got this!\n",
+          },
+          {
+            name = "CodeGPT4",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-4-1106-preview", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+          {
+            name = "CodeGPT3-5",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo-1106", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+        },
       })
+
+      vim.api.nvim_set_hl(0, "GpHandlerStandout", { link = "Normal" })
+      vim.api.nvim_set_hl(0, "GpExplorerSearch", { link = "Normal" })
     end,
   },
 }
