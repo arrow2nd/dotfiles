8c9c7e947c98c932f80bd1707c4fc5f65ca1010b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-02-16 21:58:36 +0900

    👍 (nvim) TODO,FIXME を検索するコマンド/キーマップを追加

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,2 +1,8 @@
+local create_cmd = vim.api.nvim_create_user_command
+
 -- 自動フォーマットを無視して保存
-vim.api.nvim_create_user_command("W", "noautocmd w", {})
+create_cmd("W", "noautocmd w", {})
+
+-- TODO,FIXME 一括検索
+create_cmd("Todo", "Ddu -name=grep -input=TODO:", {})
+create_cmd("Fixme", "Ddu -name=grep -input=FIXME:", {})
