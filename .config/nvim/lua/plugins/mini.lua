7175ca217d7733911d121eeb2d171e6be77094e2
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-20 12:29:33 +0900

    👍 (nvim/mini) 読み込みタイミングを変更

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -9,28 +9,30 @@ return {
         callback = function()
           -- ai
           require("mini.ai").setup({})
-          -- comment
-          require("mini.comment").setup({})
           -- splitjoin
           require("mini.splitjoin").setup({})
           -- autopair
           require("mini.pairs").setup({})
-          -- surround
-          require("mini.surround").setup({
-            mappings = {
-              add = "sa",
-              delete = "sd",
-              find = "sf",
-              find_left = "sF",
-              highlight = "sh",
-              replace = "sc",
-              update_n_lines = "sn",
-              suffix_last = "l",
-              suffix_next = "n",
-            },
-          })
         end,
         once = true,
+      })
+
+      -- comment
+      require("mini.comment").setup({})
+
+      -- surround
+      require("mini.surround").setup({
+        mappings = {
+          add = "sa",
+          delete = "sd",
+          find = "sf",
+          find_left = "sF",
+          highlight = "sh",
+          replace = "sc",
+          update_n_lines = "sn",
+          suffix_last = "l",
+          suffix_next = "n",
+        },
       })
 
       -- hipatterns
@@ -85,12 +87,12 @@ return {
               "%<", -- Mark general truncate point
               { hl = "MiniStatuslineFilename", strings = { filename() } },
               "%=", -- End left alignment
-              { hl = "MiniStatuslineFilename", strings = { get_lsp_progress(),  searchcount } },
+              { hl = "MiniStatuslineFilename", strings = { get_lsp_progress(), searchcount } },
               { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
               { hl = mode_hl, strings = { "L%l" } },
             })
           end,
-          inactive = function ()
+          inactive = function()
             local filename = function()
               if vim.bo.buftype == "terminal" then
                 return "%t"
@@ -108,23 +110,6 @@ return {
         use_icons = true,
         set_vim_settings = true,
       })
-
-      -- iceberg
-      -- local mini_statusline_colors = {
-      --   MiniStatuslineModeNormal = { bg = "#818596", fg = "#17171b" },
-      --   MiniStatuslineModeInsert = { bg = "#84a0c6", fg = "#161821" },
-      --   MiniStatuslineModeVisual = { bg = "#b4be82", fg = "#161821" },
-      --   MiniStatuslineModeReplace = { bg = "#e2a478", fg = "#161821" },
-      --   MiniStatuslineModeCommand = { bg = "#818596", fg = "#17171b" },
-      --   MiniStatuslineModeOther = { bg = "#0f1117", fg = "#3e445e" },
-      --   MiniStatuslineDevinfo = { bg = "#2e313f", fg = "#6b7089" },
-      --   MiniStatuslineFileinfo = { bg = "#2e313f", fg = "#6b7089" },
-      --   MiniStatuslineInactive = { link = "StatusLineNC" },
-      -- }
-      --
-      -- for group, conf in pairs(mini_statusline_colors) do
-      --   vim.api.nvim_set_hl(0, group, conf)
-      -- end
     end,
   },
 }
