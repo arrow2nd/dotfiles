076ea125e6798f6f13b6646259e76aa2f32422c3
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-11-21 23:46:06 +0900

    👍️: 除外するものをより正確に

--- scripts/Darwin/find.sh
+++ scripts/Darwin/find.sh
@@ -2,15 +2,14 @@
 
 # 除外するもの
 find . -type f \
-       -path "*/*" \
        -not -path "*.sh" \
        -not -path "*.md" \
-       -not -path "*scripts*" \
-       -not -path "*.git*" \
-       -not -path "*node_modules*" \
+       -not -path "*scripts/*" \
+       -not -path "*.git/*" \
+       -not -path "*node_modules/*" \
        -not -path "*.DS_Store" \
        -not -path "*.luarc.json" \
        -not -path "*stylua.toml" \
-       -not -path "*bashtop*" \
+       -not -path "*bashtop/*" \
        -not -path "*renovate.json*" \
        | cut -c 3-
--- scripts/Linux/find.sh
+++ scripts/Linux/find.sh
@@ -2,15 +2,14 @@
 
 # 除外するもの
 find . -type f \
-       -path "*/.*" \
-       -not -path "*.git*" \
-       -not -path "*node_modules*" \
+       -not -path "*.git/*" \
+       -not -path "*node_modules/*" \
        -not -path "*.DS_Store" \
        -not -path "*.Brewfile" \
        -not -path "*luarc.json" \
        -not -path "*stylua.toml" \
        -not -path "*.yabairc" \
        -not -path "*.skhdrc" \
-       -not -path "*Library*" \
+       -not -path "*Library/*" \
        -not -path "*renovate.json*" \
        | cut -c 3-
