b858243e795da2826d91042b0d3a111205333fdd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-12-19 20:34:31 +0900

    👍 (wezterm): いろいろ

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -76,22 +76,15 @@ wezterm.on("update-right-status", function(window)
 end)
 
 local keybinds = {
-  -- CTRL-S, CTRL-S で CTRL-S を送る
-  -- { key = 's', mods = 'ALT|CTRL', action = act.SendString '\x01' },
   -- コピー&ペースト
   { key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
   { key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
   -- タブ
   { key = "t", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "1", mods = "ALT", action = act.ActivateTab(0) },
-  { key = "2", mods = "ALT", action = act.ActivateTab(1) },
-  { key = "3", mods = "ALT", action = act.ActivateTab(2) },
-  { key = "4", mods = "ALT", action = act.ActivateTab(3) },
-  { key = "5", mods = "ALT", action = act.ActivateTab(4) },
-  { key = "6", mods = "ALT", action = act.ActivateTab(5) },
-  { key = "7", mods = "ALT", action = act.ActivateTab(6) },
-  { key = "8", mods = "ALT", action = act.ActivateTab(7) },
-  { key = "9", mods = "ALT", action = act.ActivateTab(8) },
+  { key = "[", mods = "ALT", action = act.ActivateTabRelative(-1) },
+  { key = "]", mods = "ALT", action = act.ActivateTabRelative(1) },
+  { key = "{", mods = "SHIFT|ALT", action = act.MoveTabRelative(-1) },
+  { key = "}", mods = "SHIFT|ALT", action = act.MoveTabRelative(1) },
   -- ペイン
   { key = "v", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
   { key = "s", mods = "ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
@@ -100,30 +93,22 @@ local keybinds = {
   { key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
   { key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
   { key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
-  {
-    key = "r",
-    mods = "ALT",
-    action = act.ActivateKeyTable({
-      name = "resize_pane",
-      one_shot = false,
-    }),
-  },
+  { key = "9", mods = "ALT", action = act.PaneSelect({ mode = "SwapWithActive" }) },
+  { key = "0", mods = "ALT", action = act.PaneSelect },
   -- その他
   { key = "Enter", mods = "ALT", action = "QuickSelect" },
   { key = "/", mods = "ALT", action = act.Search("CurrentSelectionOrEmptyString") },
   { key = "L", mods = "CTRL", action = wezterm.action.ShowDebugOverlay },
 }
 
-local key_tables = {
-  resize_pane = {
-    { key = "h", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-    { key = "j", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-    { key = "k", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-    { key = "l", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
-    { key = "Enter", action = "PopKeyTable" },
-    { key = "Escape", action = "PopKeyTable" },
-  },
-}
+-- タブ切り替え
+for i = 1, 5 do
+  table.insert(keybinds, {
+    key = tostring(i),
+    mods = "ALT",
+    action = wezterm.action.ActivateTab(i - 1),
+  })
+end
 
 local hyperlink_rules = {
   {
@@ -227,7 +212,7 @@ local config = {
   scrollback_lines = 3500,
   disable_default_key_bindings = true,
   keys = keybinds,
-  key_tables = key_tables,
+  -- key_tables = key_tables,
   hyperlink_rules = hyperlink_rules,
 }
 
