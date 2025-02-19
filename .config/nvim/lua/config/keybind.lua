9ac01c7b25f31b6dfb0b6d1dd8f3920ae7379ed6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-02-19 22:10:06 +0900

    👍 (nvim): git pushのやつ

--- .config/nvim/lua/config/keybind.lua
+++ .config/nvim/lua/config/keybind.lua
@@ -52,3 +52,4 @@ h.imap("<C-CR>", 'copilot#Accept("\\<CR>")', {
 h.nmap("<Leader>gg", "<CMD>GinStatus<CR>")
 h.nmap("<Leader>gl", "<CMD>GinLog<CR>")
 h.nmap("<Leader>gc", "<CMD>Gin commit -v<CR>")
+h.nmap("<Leader>gP", "<CMD>Gin push origin HEAD<CR>")
