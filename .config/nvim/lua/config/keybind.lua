0eb552de7cd4eefadf2a3e1d01b7cef24ffcb011
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-03-06 22:53:30 +0900

    👍 (nvim):調整

--- .config/nvim/lua/config/keybind.lua
+++ .config/nvim/lua/config/keybind.lua
@@ -55,4 +55,4 @@ h.nmap("<Leader>gc", "<CMD>Gin commit -v<CR>")
 h.nmap("<Leader>gP", "<CMD>Gin push origin HEAD<CR>")
 
 -- zen-mode.nvim
-h.nmap("<Leader>zz", "<CMD>ZenMode<CR>")
+h.nmap("<Leader>z", "<CMD>ZenMode<CR>")
