9f53c0fd3239a8e2187f0c928c781fb98275f4ad
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-05-29 09:07:18 +0900

    👍 (nvim): biomeをLSPで動かす方に寄せた

--- .config/nvim/after/lsp/efm.lua
+++ .config/nvim/after/lsp/efm.lua
@@ -32,17 +32,26 @@ local prettier = {
   rootMarkers = rootMarkers.prettier,
 }
 
-local biomefmt = {
-  formatCommand = "biome check --apply --stdin-file-path '${INPUT}'",
-  formatStdin = true,
-  requireMarker = true,
-  rootMarkers = { "rome.json", "biome.json" },
-}
-
 local denofmt = {
   formatCommand = "deno fmt --ext ${FILEEXT} -",
   formatStdin = true,
 }
+
+-- Prettier / biome / denofmt の出し分け
+local lspconfig = require("lspconfig")
+local prettier_rootdir = lspconfig.util.root_pattern(rootMarkers.prettier)
+local biome_rootdir = lspconfig.util.root_pattern(rootMarkers.biome)
+
+local denofmt_or_prettier = denofmt
+local cwd = vim.fn.getcwd()
+
+if prettier_rootdir(cwd) then
+  -- Prettier の設定がある場合は Prettier を使う
+  denofmt_or_prettier = prettier
+elseif biome_rootdir(cwd) then
+  -- biome の設定がある場合は無効に
+  denofmt_or_prettier = nil
+end
 
 local stylua = {
   formatCommand = "stylua --color Never -",
@@ -62,28 +71,27 @@ local textlint = {
   rootMarkers = rootMarkers.textlint,
 }
 
-
 return {
   init_options = {
     documentFormatting = true,
     hover = true,
     codeAction = true,
   },
-  cmd = { 'efm-langserver' },
+  cmd = { "efm-langserver" },
   settings = {
     languages = {
       html = { prettier },
       css = { prettier },
       scss = { prettier },
-      javascript = { prettier, denofmt, biomefmt },
-      javascriptreact = { prettier, biomefmt, denofmt },
-      typescript = { prettier, biomefmt, denofmt },
-      typescriptreact = { prettier, biomefmt },
-      json = { prettier, denofmt },
-      jsonc = { prettier, denofmt },
+      javascript = { denofmt_or_prettier },
+      javascriptreact = { denofmt_or_prettier },
+      typescript = { denofmt_or_prettier },
+      typescriptreact = { denofmt_or_prettier },
+      json = { denofmt_or_prettier },
+      jsonc = { denofmt_or_prettier },
       yaml = { prettier },
-      markdown = { prettier, denofmt, textlint },
+      markdown = { denofmt_or_prettier, textlint },
       lua = { stylua },
-    }
+    },
   },
 }
