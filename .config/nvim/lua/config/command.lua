3948269c54acec0b4117ce051745505317b3843c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-05-31 18:13:41 +0900

    👍 (nvim): クリップボードにテキストをコピーするコマンドを追加
    Claude CodeのREPLでSKKが使えないので、コマンドラインで書いてコピペする運用

--- .config/nvim/lua/config/command.lua
+++ .config/nvim/lua/config/command.lua
@@ -6,3 +6,14 @@ create_cmd("W", "noautocmd w", {})
 -- TODO,FIXME 一括検索
 create_cmd("Todo", "Ddu -name=grep -input=TODO:", {})
 create_cmd("Fixme", "Ddu -name=grep -input=FIXME:", {})
+
+-- クリップボードにテキストをコピー
+create_cmd("X", function(opts)
+  local text = opts.args
+
+  vim.fn.setreg("+", text)
+  vim.fn.setreg("*", text)
+  print("Copied to clipboard: " .. text)
+end, {
+  nargs = 1,
+})
