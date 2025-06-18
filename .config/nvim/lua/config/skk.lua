18a798d9575cc12ac663df1f1af1298442ba1d2d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-06-18 09:27:14 +0900

    👍 (zsh): 絵文字辞書を最後に読み込むように

--- .config/nvim/lua/config/skk.lua
+++ .config/nvim/lua/config/skk.lua
@@ -6,12 +6,22 @@ h.tmap("<C-j>", "<Plug>(skkeleton-enable)")
 
 -- 辞書を探す
 local dictionaries = {}
+local emoji_dict = nil
 local handle = io.popen("ls $HOME/.skk/*") -- フルバスで取得
 if handle then
   for file in handle:lines() do
-    table.insert(dictionaries, file)
+    if file:match("skk%-jisyo%-emoji%-ja%.utf8$") then
+      emoji_dict = file
+    else
+      table.insert(dictionaries, file)
+    end
   end
   handle:close()
+end
+
+-- 絵文字辞書を最後に追加
+if emoji_dict then
+  table.insert(dictionaries, emoji_dict)
 end
 
 vim.api.nvim_create_autocmd("User", {
