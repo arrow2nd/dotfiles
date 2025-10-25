39d8395df06ae893e0fa98361178509509c17392
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-10-25 16:36:07 +0900

    🚧 WIP

--- scripts/Darwin/find.sh
+++ scripts/Darwin/find.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+set -eu
+
 # 除外するもの
 find . -type f \
        -not -path "*.sh" \
-       -not -path "*README.md" \
+       -not -path "*.md" \
        -not -path "*scripts/*" \
        -not -path "*.git/*" \
        -not -path "*node_modules/*" \
        -not -path "*.DS_Store" \
        -not -path "*.luarc.json" \
        -not -path "*stylua.toml" \
+       -not -path "*sway*" \
+       -not -path "*waybar*" \
+       -not -path "*swaylock*" \
+       -not -path "*mako*" \
        -not -path "*bashtop/*" \
        -not -path "*renovate.json*" \
        -not -path "*.claude/settings.local.json" \
