e6796d480499dabea42b71c60e397733c739c334
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-11-07 10:29:39 +0900

    👍️ (gh): あい

--- scripts/gh.sh
+++ scripts/gh.sh
@@ -2,3 +2,7 @@
 git config --global core.editor nvim
 git config --global pull.rebase false
 git config --global ghq.root '~/workspace'
+
+# gh extensions
+gh extension install dlvhdr/gh-dash
+gh extension install arrow2nd/gh-q
