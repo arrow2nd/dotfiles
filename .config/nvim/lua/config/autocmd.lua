37c7e1fd101fb491b3694b4bb70cbe4fa1052f02
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-12-31 22:20:35 +0900

    👍 (nvim): quit時に特殊ウィンドウを閉じる

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -37,3 +37,28 @@ api.nvim_create_autocmd("LspProgress", {
 	pattern = "*",
 	command = "redrawstatus",
 })
+
+-- quitするときに特殊ウィンドウを一気に閉じる
+-- ref: https://zenn.dev/vim_jp/articles/ff6cd224fab0c7
+api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    -- 現在のウィンドウ番号を取得
+    local current_win = vim.api.nvim_get_current_win()
+    -- すべてのウィンドウをループして調べる
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      -- カレント以外を調査
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        -- buftypeが空文字（通常のバッファ）があればループ終了
+        if vim.bo[buf].buftype == '' then
+          return
+        end
+      end
+    end
+    -- ここまで来たらカレント以外がすべて特殊ウィンドウということなので
+    -- カレント以外をすべて閉じる
+    vim.cmd.only({ bang = true })
+    -- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
+  end,
+  desc = 'Close all special buffers and quit Neovim',
+})
