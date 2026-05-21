3bf3f3b81e9a43c5bc782d3eeaed088b24b8b06e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2026-05-21 18:44:38 +0900

    👍️ (nvim): もい

--- .config/nvim/lua/config/toggleterm.lua
+++ .config/nvim/lua/config/toggleterm.lua
@@ -4,3 +4,17 @@ require("toggleterm").setup()
 
 -- keymaps
 h.nmap("<Leader><Space>", "<CMD>ToggleTerm<CR>")
+h.nmap("<Leader>n", "<CMD>TermNew<CR>")
+h.nmap(";t", "<CMD>TermSelect<CR>")
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
