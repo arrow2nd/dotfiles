a1c852d9fbee71d4eb184cc6e119d8cbf2371642
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2026-09-14 00:31:37 +0900

    ♻️ リファクタ

--- .config/nvim/lua/config/autocmd.lua
+++ .config/nvim/lua/config/autocmd.lua
@@ -3,50 +3,50 @@ local augroup = api.nvim_create_augroup("AutoCommands", {})
 
 -- 外部でファイルが変更された場合に自動で再読み込み
 api.nvim_create_autocmd({ "BufEnter", "FocusGained", "CursorHold" }, {
-	group = augroup,
-	command = "checktime",
+  group = augroup,
+  command = "checktime",
 })
 
 -- rdfをxmlとして読む
 api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	group = augroup,
-	pattern = { "*.rdf" },
-	callback = function()
-		vim.opt.filetype = "xml."
+  group = augroup,
+  pattern = { "*.rdf" },
+  callback = function()
+    vim.opt.filetype = "xml."
 
-		local full_path = vim.fn.expand("%:p")
-		if string.find(full_path, "imasparql") then
-			vim.opt.filetype:append("imasrdf")
-		else
-			vim.opt.filetype:append("rdf")
-		end
-	end,
+    local full_path = vim.fn.expand("%:p")
+    if string.find(full_path, "imasparql") then
+      vim.opt.filetype:append("imasrdf")
+    else
+      vim.opt.filetype:append("rdf")
+    end
+  end,
 })
 
 -- mdxをmdとして読む
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-	pattern = "*.mdx",
-	command = "set filetype=markdown.mdx",
+  pattern = "*.mdx",
+  command = "set filetype=markdown.mdx",
 })
 
 -- 常にインサートモードでTerminalを開く
 -- ref: https://zenn.dev/ryo_kawamata/articles/improve-neovmi-terminal
 api.nvim_create_autocmd({ "TermOpen" }, {
-	group = augroup,
-	pattern = { "term://*" },
-	command = "startinsert",
+  group = augroup,
+  pattern = { "term://*" },
+  command = "startinsert",
 })
 
 -- LSPからの進行状況を受け取ったらステータスを更新する
 api.nvim_create_autocmd("LspProgress", {
-	group = augroup,
-	pattern = "*",
-	command = "redrawstatus",
+  group = augroup,
+  pattern = "*",
+  command = "redrawstatus",
 })
 
 -- quitするときに特殊ウィンドウを一気に閉じる
 -- ref: https://zenn.dev/vim_jp/articles/ff6cd224fab0c7
-api.nvim_create_autocmd('QuitPre', {
+api.nvim_create_autocmd("QuitPre", {
   callback = function()
     -- 現在のウィンドウ番号を取得
     local current_win = vim.api.nvim_get_current_win()
@@ -56,7 +56,7 @@ api.nvim_create_autocmd('QuitPre', {
       if win ~= current_win then
         local buf = vim.api.nvim_win_get_buf(win)
         -- buftypeが空文字（通常のバッファ）があればループ終了
-        if vim.bo[buf].buftype == '' then
+        if vim.bo[buf].buftype == "" then
           return
         end
       end
@@ -66,5 +66,5 @@ api.nvim_create_autocmd('QuitPre', {
     vim.cmd.only({ bang = true })
     -- この後、ウィンドウ1つの状態でquitが実行されるので、Vimが終了する
   end,
-  desc = 'Close all special buffers and quit Neovim',
+  desc = "Close all special buffers and quit Neovim",
 })
