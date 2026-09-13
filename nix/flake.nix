2fcc9c03fb499952d691e244caad0cb36c35b459
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2026-09-13 18:36:37 +0900

    📦 (nix) openscreen

--- nix/flake.nix
+++ nix/flake.nix
@@ -42,6 +42,8 @@
     llm-agents = {
       url = "github:numtide/llm-agents.nix";
     };
+
+    openscreen.url = "github:getopenscreen/openscreen";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
--- nix/home/linux/packages.nix
+++ nix/home/linux/packages.nix
@@ -1,8 +1,9 @@
-{ pkgs, linkDotfile, ... }:
+{ pkgs, inputs, linkDotfile, ... }:
 {
   home.packages = with pkgs; [
     bluetuith
     pavucontrol
+    inputs.openscreen.packages.${pkgs.stdenv.hostPlatform.system}.default
     # aarch64-darwin ではテストが落ちてビルドできず、macOS では未使用のためここに
     oxker
   ];
